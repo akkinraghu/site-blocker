ec37240db73d019980bdf1521aca426287e0711c
Raghav Akkinepally <raghav.akkinepally@nextpracticehealth.com>
2024-11-26 18:28:51 +1030

    Use tailwind instead of bootstrap

--- src/block/index.tsx
+++ src/block/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Block from './components/Block';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "../styles/global.css";
+import Block from "./components/Block";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 if (!container) {
-  throw new Error('Root element not found');
+  throw new Error("Root element not found");
 }
 
 const root = createRoot(container);
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Popup from './components/Popup';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "../styles/global.css";
+import Popup from "./components/Popup";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 if (!container) {
-  throw new Error('Root element not found');
+  throw new Error("Root element not found");
 }
 
 const root = createRoot(container);
